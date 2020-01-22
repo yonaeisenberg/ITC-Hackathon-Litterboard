5d263fe6ea0bd726b2309e73a534e496b57eb0c7
arielle <arielleoren@gmail.com>
2020-01-22 16:34:26 +0200

    changed event list

--- react/src/components/eventlist.jsx
+++ react/src/components/eventlist.jsx
@@ -1,10 +1,63 @@
-import React from "react"
+import React from 'react'
+import { Table } from 'react-bootstrap';
+import { render } from '@testing-library/react';
 
-function EventList(){
-
-    return(
-        <h1>EventList placeholder</h1>
+class EventList extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    counter ++;
+  }
+  
+render() {
+  <counter count={0}/>
+    return (
+        <div>
+        <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Area</th>
+            <th>Votes</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Gordon Beach</td>
+            <td>5</td>
+            <td><button onClick={this.add}> + </button></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Florentine</td>
+            <td>6</td>
+            <td><button onClick={this.add}> + </button></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>old north</td>
+            <td>6</td>
+            <td><button onClick={this.add}> + </button></td>
+          </tr>
+          <tr>
+          <td>4</td>
+          <td>shuk ha carmel</td>
+          <td>6</td>
+          <td><button onClick={this.add}> + </button></td>
+        </tr>
+        <tr>
+        <td>5</td>
+        <td>jaffa beach</td>
+        <td>10</td>
+        <td><button onClick={this.add}> + </button></td>
+      </tr>
+        </tbody>
+      </Table>
+        </div>
     )
+}
 }
 
 export default EventList
