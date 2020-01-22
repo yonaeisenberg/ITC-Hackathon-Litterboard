10efcffe3843de794bf755b540204f7cff22b76b
yona_eisenberg <jonas.eisenberg@gmail.com>
2020-01-22 17:48:07 +0200

    Add CORS to flask

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from FetchBoard import fetchboard_app
 from AddRow import addrow_app
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(fetchboard_app)
 app.register_blueprint(addrow_app)
