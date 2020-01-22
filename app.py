7836b289cefc4f5a039854f5b333f108b7a6df3b
AvnerADDA <addavner@gmail.com>
2020-01-22 20:26:44 +0200

    Add place to website

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from FetchBoard import fetchboard_app
 from AddRow import addrow_app
+from Maps_insert import place_app
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(fetchboard_app)
 app.register_blueprint(addrow_app)
+app.register_blueprint(place_app)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=7000)
